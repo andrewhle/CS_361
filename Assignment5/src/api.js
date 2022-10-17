c023847dfe7d42424e1b74668e31d38b977b1639
Hung Le <andrewle0915@gmail.com>
2022-10-17 16:44:01 -0700

    First Commit

---
+++ Assignment5/src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "b370dfbc7cmshcdba3fa8ed0a8d7p11f9dbjsn8bad05ef687d",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "d9d8c44994c628b014ab600a1ee95045";
